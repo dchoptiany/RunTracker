07ba670e432e978f0f4b99a6c709c08674062b78
Aleksandra <aleksandra.zaremba02@gmail.com>
2023-04-29 21:52:59 +0200

    Integrate map with app
    - Add library (without it, viewModels was unresolved in my project)
    - Add required permissions for MapView

--- app/src/main/java/com/example/runtracker/MapFragment.kt
+++ app/src/main/java/com/example/runtracker/MapFragment.kt
@@ -1,21 +1,31 @@
 package com.example.runtracker
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
+import org.osmdroid.config.Configuration;
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.clans.fab.FloatingActionButton
+import org.osmdroid.api.IMapController
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MapFragment : Fragment() {
     lateinit var addPhotoFAButton : FloatingActionButton
     lateinit var startButton : ImageButton
     lateinit var pauseButton : ImageButton
     lateinit var stopButton : ImageButton
+    lateinit var mapView : MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +52,36 @@ class MapFragment : Fragment() {
         stopButton = view!!.findViewById(R.id.stopButton) as ImageButton
         stopButton.setOnClickListener {
             stopActivity()
+        }
+
+        mapView = view!!.findViewById(R.id.mapView) as MapView
+
+        var context = requireActivity().applicationContext
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context))
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+
+        mapView.setUseDataConnection(true)
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+        mapView.setMultiTouchControls(true)
+
+        val mapController : IMapController = mapView.controller
+        mapController.zoomTo(14, 1)
+        var defaultLocation : GeoPoint = GeoPoint(51.1077,17.0625) // Wrocław University of Science and Technlogy
+        mapController.animateTo(defaultLocation)
+
+        val myGpsMyLocationProvider = GpsMyLocationProvider(activity)
+        val myLocationOverlay = MyLocationNewOverlay(myGpsMyLocationProvider, mapView)
+        myLocationOverlay.enableMyLocation()
+        myLocationOverlay.enableFollowLocation()
+
+        val icon = BitmapFactory.decodeResource(resources, org.osmdroid.library.R.drawable.ic_menu_compass)
+        myLocationOverlay.setPersonIcon(icon)
+        mapView.overlays.add(myLocationOverlay)
+
+        myLocationOverlay.runOnFirstFix {
+            mapView.overlays.clear()
+            mapView.overlays.add(myLocationOverlay)
+            mapController.animateTo(myLocationOverlay.myLocation)
         }
 
         return view
