52cfa8474b09080cdb0ef8e2d672e35be4352875
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-04-29 14:29:52 +0200

    Added static database instance access

--- app/src/main/java/com/example/runtracker/AppDatabase.kt
+++ app/src/main/java/com/example/runtracker/AppDatabase.kt
@@ -1,9 +1,28 @@
 package com.example.runtracker
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Run::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun runDao(): RunDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
