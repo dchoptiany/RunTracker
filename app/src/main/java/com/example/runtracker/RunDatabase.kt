ebd4654338b40f83239d46817d624c0df6101c3b
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-04-29 19:15:15 +0200

    Renamed and updated AppDatabase

--- app/src/main/java/com/example/runtracker/RunDatabase.kt
+++ app/src/main/java/com/example/runtracker/RunDatabase.kt
@@ -5,21 +5,25 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.sql.Date
 
-@Database(entities = [Run::class], version = 1)
+@Database(entities = [Run::class], version = 1, exportSchema = false)
 @TypeConverters(DateConverter::class)
-abstract class AppDatabase : RoomDatabase() {
+abstract class RunDatabase : RoomDatabase() {
     abstract fun runDao(): RunDao
 
     companion object {
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+        private var INSTANCE: RunDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase {
+        fun getDatabase(context: Context): RunDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    AppDatabase::class.java,
+                    RunDatabase::class.java,
                     "database"
                 ).build()
                 INSTANCE = instance
