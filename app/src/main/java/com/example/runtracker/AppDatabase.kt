4e2bc827079b53ff3b295107f404c687599621ed
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-04-29 14:37:36 +0200

    Added TypeConverter for Date

--- app/src/main/java/com/example/runtracker/AppDatabase.kt
+++ app/src/main/java/com/example/runtracker/AppDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Run::class], version = 1)
+@TypeConverters(DateConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun runDao(): RunDao
 
