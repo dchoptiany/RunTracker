85e44f9f3766662012cb5464238d7176fc839f4d
woycik <88285570+woycik@users.noreply.github.com>
2023-05-01 19:37:42 +0200

    Update ColorPicker.kt

--- app/src/main/java/com/example/runtracker/ColorPicker.kt
+++ app/src/main/java/com/example/runtracker/ColorPicker.kt
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity
 class ColorPicker: AppCompatActivity() {
 
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.color)
