65d82fc1909831b894e117bf5b6d3340fb0cf3e1
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-05-16 11:58:33 +0200

    Added HistoryActivity

---
+++ app/src/main/java/com/example/runtracker/HistoryActivity.kt
@@ -0,0 +1,47 @@
+package com.example.runtracker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class HistoryActivity : AppCompatActivity(), RunHistoryAdapter.OnRunItemClickListener {
+    private lateinit var recyclerViewHistory: RecyclerView
+
+    private val runViewModel: RunViewModel by viewModels {
+        RunModelFactory((application as RunApplication).repository)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        recyclerViewHistory = findViewById(R.id.recyclerViewHistory)
+        updateRecyclerView()
+    }
+
+    private fun getRunHistoryItems(): ArrayList<RunHistoryItem> {
+        val runHistoryItems = ArrayList<RunHistoryItem>()
+
+        for(run in runViewModel.runs.value!!) {
+            runHistoryItems.add(RunHistoryItem(run.id, run.date, run.distance, run.duration))
+        }
+
+        return runHistoryItems
+    }
+
+    private fun updateRecyclerView() {
+        recyclerViewHistory.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerViewHistory.adapter = RunHistoryAdapter(getRunHistoryItems(), this)
+    }
+
+    override fun onClick(position: Int, runID: Int) {
+        Intent(this, SummaryActivity::class.java).apply {
+            putExtra("runID", runID)
+        }.also {
+            startActivity(it)
+        }
+    }
+}
