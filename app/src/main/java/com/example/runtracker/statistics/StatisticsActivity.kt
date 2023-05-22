951e4dc5a9203ea0ec07b2bd95444ab3860e70e9
Damian Choptiany <56999385+dchoptiany@users.noreply.github.com>
2023-05-22 13:43:20 +0200

    Added viewModel binding

--- app/src/main/java/com/example/runtracker/statistics/StatisticsActivity.kt
+++ app/src/main/java/com/example/runtracker/statistics/StatisticsActivity.kt
@@ -17,5 +17,33 @@ class StatisticsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+
+        viewModel.numberOfRuns.observe(this) {
+            findViewById<TextView>(R.id.textViewTotalRuns).text = it.toString()
+        }
+
+        viewModel.totalDistance.observe(this) {
+            findViewById<TextView>(R.id.textViewTotalDistance).text = it.toString()
+        }
+
+        viewModel.totalDuration.observe(this) {
+            findViewById<TextView>(R.id.textViewTotalDuration).text = it.toString()
+        }
+
+        /*viewModel.totalPhotos.observe(this) {
+            findViewById<TextView>(R.id.textViewTotalPhotos).text = it.toString()
+        }*/
+
+        viewModel.longestDistance.observe(this) {
+            findViewById<TextView>(R.id.textViewLongestDistance).text = it.toString()
+        }
+
+        viewModel.longestDuration.observe(this) {
+            findViewById<TextView>(R.id.textViewLongestDuration).text = it.toString()
+        }
+
+        /*viewModel.mostPhotosInRun.observe(this) {
+            findViewById<TextView>(R.id.textViewMostPhotos).text = it.toString()
+        }*/
     }
 }
